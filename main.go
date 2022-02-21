28808881b511a8a115d8c335131e014bd039980a
nuryudha <nur.yudha16@gmail.com>
2022-02-21 09:03:52 +0700

    add feature A

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ func main(){
 	fmt.Println("Hello World")
 	fmt.Println("A")
 	fmt.Println("B")
-<<<<<<< Updated upstream
 	fmt.Println("F")
-=======
 	fmt.Println("C")
 	fmt.Println("D")
->>>>>>> Stashed changes
+	fmt.Println("Feature A")
 }
